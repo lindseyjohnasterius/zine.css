fad346c19fd829196fbe5b5aaa2236208dc51ff8
LNSY <lindsey.mysse@gmail.com>
2021-09-09 13:28:48 -0700

    zine css

--- README.md
+++ README.md
@@ -8,3 +8,37 @@ A hybrid Zine / Web Site concept. When it is done should allow you to author a z
 # Patreon
 
 Like my code? Support my patreon: https://www.patreon.com/LNSY
+
+```HTML
+
+<zine-wrapper>
+	<zine-cover>
+		<!-- Front and back of the Zine respectively -->
+		<zine-front></zine-front>
+		<zine-back></zine-back>
+	</zine-cover>
+
+	<zine-spread>
+		<zine-left-page page="page-inside-cover"></zine-left-page>
+		<zine-right-page></zine-right-page>
+	</zine-spread>
+
+	<zine-spread>
+		<zine-left-page page-number="3"></zine-left-page>
+		<zine-right-page page-number="4"></zine-right-page>
+	</zine-spread>
+
+	<zine-spread>
+		<zine-left-page page-number="5"></zine-left-page>
+		<zine-right-page page-number="6"></zine-right-page>
+	</zine-spread>
+
+	<zine-spread>
+		<zine-left-page page-number="7"></zine-left-page>
+		<zine-right-page page-number="8"></zine-right-page>
+	</zine-spread>
+
+
+</zine-wrapper>
+
+```
--- zine.js
+++ zine.js
@@ -4,201 +4,59 @@ function dispatch(name, detail = {}, div = document){
 }
 
 
+let counter = 0 
+
+
 document.addEventListener('GO TO PAGE', (e) => {
-  const width = window.innerWidth  
-  const height = window.innerHeight
-
-  const zine_wrapper =document.querySelector('zine-wrapper')
-  zine_wrapper.requestFullscreen()
-
-  if(height > width){
-    
-    switch(e.detail){
-      case "1":
-        zine_wrapper.scrollTo({left:0,top:height,behavior:'smooth'})
-        break;
-      case "2":
-        zine_wrapper.scrollTo({left:width, top:height,behavior:'smooth'})
-        break;
-      case "3":
-        zine_wrapper.scrollTo({left:0, top:height * 2,behavior:'smooth'})
-        break;
-      case "4":
-        zine_wrapper.scrollTo({left:width,top: height * 2,behavior:'smooth'})
-        break;
-      case "5":
-        zine_wrapper.scrollTo({left:0,top: height * 3,behavior:'smooth'})
-        break;
-      case "6":
-        zine_wrapper.scrollTo({left:width, top:height * 3,behavior:'smooth'})
-        break;
-      case "7":
-        zine_wrapper.scrollTo({left:0,top: height * 4,behavior:'smooth'})
-        break;
-      case "BACK COVER":
-        zine_wrapper.scrollTo({left:width, top:0,behavior:'smooth'});
-        break;
-      case "COVER": 
-      default:
-        zine_wrapper.scrollTo({left:0,top:0,behavior:'smooth'})
-    }
-
-  } else {
-    switch(e.detail){
-      case "1":
-        zine_wrapper.scrollTo({left:0,top:height,behavior:'smooth'})
-        break;
-      case "2":
-        zine_wrapper.scrollTo({left:0, top:height,behavior:'smooth'})
-        break;
-      case "3":
-        zine_wrapper.scrollTo({left:0, top:height * 2,behavior:'smooth'})
-        break;
-      case "4":
-        zine_wrapper.scrollTo({left:0,top: height * 2,behavior:'smooth'})
-        break;
-      case "5":
-        zine_wrapper.scrollTo({left:0,top: height * 3,behavior:'smooth'})
-        break;
-      case "6":
-        zine_wrapper.scrollTo({left:0, top:height * 3,behavior:'smooth'})
-        break;
-      case "7":
-        zine_wrapper.scrollTo({left:0,top: height * 4,behavior:'smooth'})
-        break;
-      case "BACK COVER":
-        zine_wrapper.scrollTo({left:0, top:0,behavior:'smooth'});
-        break;
-      case "COVER": 
-      default:
-        zine_wrapper.scrollTo({left:0,top:0,behavior:'smooth'})
-    }
-  }
-  
-  
+  console.log('GO TO PAGE', e.detail)
+  const target = document.querySelector(`#${e.detail}`)
+  const top = target.offsetTop
+  const left = 0
+  document.querySelector('zine-wrapper').scrollTo({top, left, behavior:'smooth'})
 })
 
 document.addEventListener('NEXT PAGE', (e) => {
-  const zine_wrapper = document.querySelector('zine-wrapper')
-  zine_wrapper.requestFullscreen()
+  counter++
 
-  const width = window.innerWidth  
-  const height = window.innerHeight
+  if(counter > 6) counter = 0
+  const zine_controls = document.querySelector('#zine-controls')
+  const zine_controls_buttons = [...zine_controls.querySelectorAll('button')]
+  
+  zine_controls_buttons[counter].click()
 
-  const current_left = zine_wrapper.scrollLeft
-  const current_top = zine_wrapper.scrollTop
-  
-  let left = 0
-  let top = 0
-  
-  if(current_left === 0){
-    left = width
-    top = current_top
-  } else {
-    left = 0
-    top = current_top + height
-    if(top > height * 3) top = 0
-  }
-
-  if(width > height){
-    left = 0
-    top = current_top + height
-    if(top > height * 3) top = 0
-
-  }
-  
-  zine_wrapper.scrollTo({left, top, behavior:'smooth'})
-  
 })
 
 document.addEventListener('PREV PAGE', (e) => {
-  const zine_wrapper = document.querySelector('zine-wrapper')
-  zine_wrapper.requestFullscreen()
-
-  const width = window.innerWidth  
-  const height = window.innerHeight
-
-  const current_left = zine_wrapper.scrollLeft
-  const current_top = zine_wrapper.scrollTop
-  
-  let left = 0
-  let top = 0
-  
-  if(current_left === width){
-    left = 0
-    top = current_top
-  } else {
-    left = width
-    top = current_top - height
-    if(top < 0) top = 0
-  }
-
-if(width > height){
-  left = 0
-  top = current_top - height
-  if(top < 0) top = 0
-}
-  
-  zine_wrapper.scrollTo({left, top, behavior:'smooth'})
-
+  counter--
+  if(counter < 0) counter = 6
+  const zine_controls = document.querySelector('#zine-controls')
+  const zine_controls_buttons = [...zine_controls.querySelectorAll('button')]  
+  zine_controls_buttons[counter].click()
 })
-
-
 
 class ZineWrapper extends HTMLElement {
   connectedCallback(){
     const zine_controls = document.createElement('zine-controls')
     this.prepend(zine_controls)
-
   }
-
-  static get observedAttributes() {
-    return [];
-  }
-
-  attributeChangedCallback(name, old_value, new_value){
-    switch(name){
-      default:
-    }
-  }
-
 }
-
-customElements.define('zine-wrapper', ZineWrapper)
-
-
-
-
-
 
 class ZineControls extends HTMLElement {
   connectedCallback(){
     this.innerHTML = `<button onclick="dispatch('PREV PAGE')" style="position: fixed; left:1em; bottom:1em; z-index:9000;">Prev</button>
     <button onclick="dispatch('NEXT PAGE')" style="position: fixed; right:1em; bottom:1em; z-index:9000;">Next</button>
   
-    <details style="position:fixed;left:1em;top:1em;z-index:9000;">
+    <details id="zine-controls" style="position:fixed;left:1em;top:1em;z-index:9000;">
       <summary> </summary>
-      <button onclick="dispatch('GO TO PAGE','COVER')">Cover</button><br>
-      <button onclick="dispatch('GO TO PAGE','1')">1</button><br>
-      <button onclick="dispatch('GO TO PAGE','2')">2</button><br>
-      <button onclick="dispatch('GO TO PAGE','3')">3</button><br>
-      <button onclick="dispatch('GO TO PAGE','4')">4</button><br>
-      <button onclick="dispatch('GO TO PAGE','5')">5</button><br>
-      <button onclick="dispatch('GO TO PAGE','6')">6</button><br>
-      <button onclick="dispatch('GO TO PAGE','BACK COVER')">Back Cover</button>
-
+      <button onclick="dispatch('GO TO PAGE','front-page')">front-page</button><br>
+      <button onclick="dispatch('GO TO PAGE','page-1')">Page 1</button><br>
+      <button onclick="dispatch('GO TO PAGE','page-2')">Page 2</button><br>
+      <button onclick="dispatch('GO TO PAGE','inside-spread')">Inside Spread</button><br>
+      <button onclick="dispatch('GO TO PAGE','page-5')">Page 5</button><br>
+      <button onclick="dispatch('GO TO PAGE','page-6')">Page 6</button><br>
+      <button onclick="dispatch('GO TO PAGE','back-page')">Back Cover</button>
     </details>
 `
-  }
-
-  static get observedAttributes() {
-    return [];
-  }
-
-  attributeChangedCallback(name, old_value, new_value){
-    switch(name){
-      default:
-    }
   }
 
 }
@@ -207,88 +65,53 @@ customElements.define('zine-controls', ZineControls)
 
 
 
-
-class ZineSpread extends HTMLElement {
-  connectedCallback(){
-  }
-
-  static get observedAttributes() {
-    return [];
-  }
-
-  attributeChangedCallback(name, old_value, new_value){
-    switch(name){
-      default:
-    }
-  }
-
-}
-
-customElements.define('zine-spread', ZineSpread)
-
-
-
-
-class ZineCover extends ZineSpread {
-
-}
-
-customElements.define('zine-cover', ZineCover)
-
-
-
-
-
-class ZineLeftPage extends HTMLElement {
-  connectedCallback(){
-  }
-
-  static get observedAttributes() {
-    return [];
-  }
-
-  attributeChangedCallback(name, old_value, new_value){
-    switch(name){
-      default:
-    }
-  }
-
-}
-
-customElements.define('zine-left-page', ZineLeftPage)
-
-
-
-
-
-
-class ZineRightPage extends ZineLeftPage {
-
-}
-
-customElements.define('zine-right-page', ZineRightPage)
-
-
-
-
+customElements.define('zine-wrapper', ZineWrapper)
 
 
 class ZinePageContent extends HTMLElement {
   connectedCallback(){
   }
-
-  static get observedAttributes() {
-    return [];
-  }
-
-  attributeChangedCallback(name, old_value, new_value){
-    switch(name){
-      default:
-    }
-  }
-
 }
 
 customElements.define('zine-page-content', ZinePageContent)
+
+
+class ZinePage extends HTMLElement {
+  connectedCallback(){
+    this.id = this.getAttribute('id')
+    fetch(this.id + '.html')
+    .then(res => res.text())
+    .then(res => {
+      const zine_content = document.createElement('zine-page-content')
+      zine_content.innerHTML = res
+      this.appendChild(zine_content)
+    })
+  }
+}
+
+customElements.define('zine-page', ZinePage)
+
+
+class ZineSpreadPage extends HTMLElement {
+  connectedCallback(){
+    this.id = this.getAttribute('id')
+    fetch(this.id + '.html')
+    .then(res => res.text())
+    .then(res => {
+      const zine_content = document.createElement('zine-page-content')
+      zine_content.classList.add('spread')
+      zine_content.innerHTML = res
+      this.appendChild(zine_content)
+    })
+
+  }
+}
+
+customElements.define('zine-spread-page', ZineSpreadPage)
+
+
+
+
+
 
 
